835f32877097d6ef4572f225566d5fbc91be335f
Muhammad Haroon <theharoonsocial@gmail.com>
2024-04-17 23:53:44 +0500

    complete featured section

---
+++ src/app/components/FeaturedSection.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React from "react";
+import { BackgroundGradient } from "@/app/components/ui/background-gradient";
+// import { IconAppWindow } from "@tabler/icons-react";
+import data from "../../../data.json";
+
+interface featuredDataType {
+  id: number;
+  name: string;
+  age: number;
+  instrument: string;
+  description: string;
+  lesson: string;
+  level: string;
+}
+
+const FeaturedSection = () => {
+  const featuredData = data.filter((item: featuredDataType) => item.age > 35);
+  return (
+    <div className="bg-slate-900 min-h-screen py-20 px-10">
+      <h2 className="font-bold text-lg text-center text-green-700 uppercase">
+        Featured Section
+      </h2>
+      <h3 className="text-center font-bold text-4xl text-white capitalize mt-10">
+        Learn with the best
+      </h3>
+
+      <div className="flex flex-wrap gap-10 mt-10 items-center">
+        {featuredData.map((item: featuredDataType) => (
+          <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900 w-[400px]">
+            <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+              {item.lesson}
+            </p>
+
+            <p className="text-sm text-neutral-600 dark:text-neutral-400">
+              {item.description}
+            </p>
+            <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+              <span>Learn More </span>
+              <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+                →
+              </span>
+            </button>
+          </BackgroundGradient>
+        ))}
+      </div>
+
+      <div className="flex justify-center mt-10">
+        <button className="rounded-full px-4 pr-1 py-5 text-white flex items-center space-x-1 bg-black text-xs font-bold dark:bg-zinc-800">
+          <span className="text-sm">View All Lessons </span>
+          <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+            →
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedSection;
--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -5,30 +5,32 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <HeroHighlight>
-      <div className="text-center p-4 py-10">
-        <h1 className="mt-20 py-10 text-4xl md:text-7xl font-bold text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text">
-          CodexHaroon x AceternityUI
-        </h1>
-        <p className="mx-auto max-w-lg mt-4 mb-4 text-base sm:text-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-          veritatis{" "}
-          <Highlight>
-            repudiandae blanditiis cum voluptas commodi cupiditate
-          </Highlight>
-          corrupti ut perspiciatis laborum nesciunt recusandae adipisci, earum
-          tempore natus consequatur fuga. Voluptatum, dolores!
-        </p>
-        <Link href={"/"}>
-          <Button
-            borderRadius="1.75rem"
-            className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-          >
-            Yo Button!
-          </Button>
-        </Link>
-      </div>
-    </HeroHighlight>
+    <div className="min-h-screen">
+      <HeroHighlight>
+        <div className="text-center p-4 py-10">
+          <h1 className="mt-20 py-10 text-4xl md:text-7xl font-bold text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text">
+            CodexHaroon x AceternityUI
+          </h1>
+          <p className="mx-auto max-w-lg mt-4 mb-4 text-base sm:text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            veritatis{" "}
+            <Highlight>
+              repudiandae blanditiis cum voluptas commodi cupiditate
+            </Highlight>
+            corrupti ut perspiciatis laborum nesciunt recusandae adipisci, earum
+            tempore natus consequatur fuga. Voluptatum, dolores!
+          </p>
+          <Link href={"/"}>
+            <Button
+              borderRadius="1.75rem"
+              className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Yo Button!
+            </Button>
+          </Link>
+        </div>
+      </HeroHighlight>
+    </div>
   );
 };
 
