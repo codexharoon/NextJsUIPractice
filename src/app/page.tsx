1294ee727dbe625679c7642cdb9a3405e96825c8
Muhammad Haroon <theharoonsocial@gmail.com>
2024-04-19 18:29:54 +0500

    Improve ui + add moving card section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import HeroSection from "./components/HeroSection";
 import FeaturedSection from "./components/FeaturedSection";
 import WhyUs from "./components/WhyUs";
+import MovingCardSection from "./components/MovingCardSection";
 
 const page = () => {
   return (
-    <>
+    <main className="min-h-screen bg-black/[0.96] antialiased">
       <HeroSection />
       <FeaturedSection />
       <WhyUs />
-    </>
+      <MovingCardSection />
+    </main>
   );
 };
 
